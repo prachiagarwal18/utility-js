fccc59b9aabf63f725791207b245e41bbb8ea6c0
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 21:59:56 +0530

    Update map.js

--- src/map.js
+++ src/map.js
@@ -1,31 +1,9 @@
-const map = (array,function_performed)=>{
-    if(array.length==0){
-        return [];
-    }
-    else if(function_performed== 'identity'){
-        return identity(array);
-    }
-    else if(function_performed=='cube'){
-        return cube(array);
-    }
-    else{
-        return array+1;
-    }
-}
-const identity= (array) =>{
-    return array;
-}
-const cube =(array) =>{
-    var arr_new=[],index=0;
-    array.forEach(element => {
-        arr_new[index]=element*element*element;
-        index++;
+const head = require('./head');
+const tail = require('./tail');
 
-    });
-    return arr_new;
-}
-
+const map = (list, callback) => 
+    head(list) === undefined 
+        ? [] 
+        : [callback(head(list)),...map(tail(list), callback)];
 
 module.exports = map;
-
-
