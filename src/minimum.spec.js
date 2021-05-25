acee3168073f89ec8c684a30745d364eed2c46e2
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 17:57:31 +0530

    Update minimum.spec.js

--- src/minimum.spec.js
+++ src/minimum.spec.js
@@ -4,4 +4,7 @@ describe('Minimum',()=>{
     it('minimum of empty array is undefined',()=>{
         expect(minimum([],0)).toEqual(undefined);
     })
+    it('Minimum of [2,4,3] should be 2',()=>{
+        expect(minimum([2,4,3],3)).toEqual(2);
+    })
 })
