18cafd044296b604881bd0e35f20fa03a04a00f8
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 15:51:43 +0530

    Update tail.spec.js

--- src/tail.spec.js
+++ src/tail.spec.js
@@ -4,5 +4,9 @@ describe('Tail',()=>{
         var array = []
         expect(tail(array)).toEqual(undefined);
     });
+   it('Should return array after reoving the first element',()=>{
+                var arr=[1,2,3,4]
+                expect(tail(arr)).toEqual([2,3,4]);
+            });
    
 })
