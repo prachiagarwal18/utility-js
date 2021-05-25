a7c238651299806df50e0f938b06c2f59c8ae792
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 19:30:04 +0530

    Update map.spec.js

--- src/map.spec.js
+++ src/map.spec.js
@@ -21,29 +21,3 @@ describe('Map',() => {
 
 
 
-// const map = require('./map');
-
-// describe('Map', () => {
-    
-//     it('Map of empty list should return empty list', () => {
-//         let list = []
-//         expect(map(list, x => x**3)).toEqual([]);
-//     });
-
-//     it('Map of [1,2,3] with cube function should give [1,8,27]', () => {
-//         let list = [1,2,3]
-//         expect(map(list, x => x**3)).toEqual([1,8,27]);
-//     });
-
-//     it('Map of [1,2,3] with Identity function is [1,2,3]', () => {
-//         let list = [1,2,3]
-//         expect(map(list, x => x)).toEqual([1,2,3]);
-//     });
-    
-    // it('Map of a.x with a.x+1 should give 11', () => {
-    //     let a = {x:10}
-    //     expect(map([a],Obj => Obj.x + 1)).toEqual([11]);
-    // });
-    
-
-// })
