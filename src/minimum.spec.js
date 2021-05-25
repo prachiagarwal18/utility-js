7a6e0133170d4fbc21d096c300d871c243f8892e
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 17:59:48 +0530

    adding test case

--- src/minimum.spec.js
+++ src/minimum.spec.js
@@ -7,4 +7,8 @@ describe('Minimum',()=>{
     it('Minimum of [2,4,3] should be 2',()=>{
         expect(minimum([2,4,3],3)).toEqual(2);
     })
+    it('Minimum of [2,4,3] should be 2',()=>{
+        var testArray=[-11,2,3,4,0]
+        expect(minimum(testArray,testArray.length)).toEqual(-11);
+    })
 })
