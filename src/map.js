02980b6143cbdec25bb6d4bce49c9bf889ec41df
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 19:26:23 +0530

    Update map.js

--- src/map.js
+++ src/map.js
@@ -29,23 +29,3 @@ const cube =(array) =>{
 module.exports = map;
 
 
-// const head = require('./head')
-// const tail = require('./tail')
-// let resultingList=[]
-// const compute= (callbackFunction,list,listLength)=>{
-//     if (resultingList.length!=listLength){
-//     if(list.length > 0) {
-//         resultingList.push(callbackFunction(head(list)));
-//         return compute(callbackFunction,tail(list),listLength)
-//     }}
-//     let answer = resultingList
-//     resultingList=[]
-//     return answer 
-// }
-// const map = (list, callbackFunction) => {
-//     var listLength = list.length
-//     let result = compute(callbackFunction,list,listLength)
-//     return result;
-// }
-
-// module.exports = map;
