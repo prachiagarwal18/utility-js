f40b9e227a1d00bd8b83939194aeae9ed2f49ac3
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 16:18:10 +0530

    Update maximum.spec.js

--- src/maximum.spec.js
+++ src/maximum.spec.js
@@ -3,4 +3,8 @@ describe('Maximum',()=>{
     it('Maximum of [] is undefined',()=>{
         expect(maximum([])).toEqual(undefined);
     })
+    it('Maximum element in [2,66,3,55,123,9,7,0] is 123',()=>{
+        var a=[2,66,3,55,123,9,7,0];
+        expect(maximum(a,a.length)).toEqual(123);
+    })
 })
