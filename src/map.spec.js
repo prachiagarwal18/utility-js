7180f81abc625d2be2f52fb5c1e55cb613e9724d
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 22:00:37 +0530

    Update map.spec.js

--- src/map.spec.js
+++ src/map.spec.js
@@ -1,23 +1,19 @@
 const map = require('./map');
-
+const cube=(number)=>{return number*number*number};
+const identity= (number) => {return number};
 describe('Map',() => {
     it('Map of empty array shuld return empty array',()=>{
-        expect(map([],'cube')).toEqual([]);
+        expect(map([],cube)).toEqual([]);
     });
 
     it('Map of [2,3,4],cube is [8,27,64]',()=>{
-        expect(map([2,3,4],'cube')).toEqual([8,27,64]);
+        expect(map([2,3,4],cube)).toEqual([8,27,64]);
     });
     it('Map of [1,2,3,4],identity is [1,2,3,4]',()=>{
-            expect(map([1,2,3,4],'identity')).toEqual([1,2,3,4]);
+            expect(map([1,2,3,4],identity)).toEqual([1,2,3,4]);
     });
-    it('Map of a.x with a.x+1 should give 11', () => {
-        
-        expect(map(10)).toEqual(11);
+    it('map([a{x : 10}],someObject => someObject.x + 1)  -> [11]', () => {
+        expect(map([{ x: 10 }], someObject => someObject.x + 1)).toEqual([11]);  
+        // expect(map(10)).toEqual(11);
     });
 })
-
-
-
-
-
