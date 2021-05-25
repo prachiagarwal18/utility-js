5a7df814d4d97288ed1c907b1eb4547929e038d5
prachiagarwal18 <37240289+prachiagarwal18@users.noreply.github.com>
2021-05-25 16:18:59 +0530

    Update maximum.js

--- src/maximum.js
+++ src/maximum.js
@@ -1,10 +1,12 @@
-var array=[1,2,3,4]
-const maximum= (array)=>{
-    if (array.length==0) {
+const maxElement = (number1, number2) => (number1>number2)?number1:number2;
+
+const maximum = (array, size) => {
+    if(size==0)
         return undefined;
-    }
-    else{
-        return Math.max.apply(array);
-    }
-}
-module.exports=maximum;
+    else if( size == 1)
+        return array[0];
+    else
+        return maxElement(array[size], maximum(array, size - 1));
+} 
+
+module.exports = maximum;
